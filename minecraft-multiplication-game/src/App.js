595320077cfccca7d9e35e0d390b6526f538bc10
Hidenori Tanaka <hidenori@dhcp-10-250-65-176.harvard.edu>
2024-09-09 09:37:26 -0400

    Initial commit

---
+++ minecraft-multiplication-game/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MinecraftMultiplicationGame from './MinecraftMultiplicationGame';
+
+function App() {
+  return (
+    <div className="App">
+      <MinecraftMultiplicationGame />
+    </div>
+  );
+}
+
+export default App;
